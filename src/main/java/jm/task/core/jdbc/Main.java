b892f89f7e3e60d14ef6a33e87ef0b47619bc0a7
Alexandr <yakulaginalexandr@ya.ru>
2024-10-31 17:46:35 +0300

    main solution

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,19 +4,24 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
-        User user1 = new User("Максим", "Николаев", (byte) 24);
-        User user2 = new User("Маргарита", "Лебедева", (byte) 21);
-        User user3 = new User("Олег", "Коровин", (byte) 35);
-        User user4 = new User("Матвей", "Морозов", (byte) 27);
-        userService.saveUser(user1);
-        userService.saveUser(user2);
-        userService.saveUser(user3);
-        userService.saveUser(user4);
+
+        List<User> userList = new LinkedList<>();
+        userList.add(new User("Максим", "Николаев", (byte) 24));
+        userList.add( new User("Маргарита", "Лебедева", (byte) 21));
+        userList.add( new User("Олег", "Коровин", (byte) 35));
+        userList.add(new User("Матвей", "Морозов", (byte) 27));
+
+        userList.forEach(userService::saveUser);
+
         System.out.println(userService.getAllUsers());
+
         userService.cleanUsersTable();
         userService.dropUsersTable();
     }
